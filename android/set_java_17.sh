bd9dcee065454e43025bb225c59b3c5e5b694a7f
tuanphan1806 <22010282@st.phenikaa-uni.edu.vn>
2025-02-22 17:13:07 +0700

    push base project

---
+++ android/set_java_17.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+export PATH=$JAVA_HOME/bin:$PATH
+
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Flutter
+import Firebase
+import fluttertoast 
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Initialize Firebase
+    FirebaseApp.configure()
+
+    // Register plugins
+    GeneratedPluginRegistrant.register(with: self)
+    
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
